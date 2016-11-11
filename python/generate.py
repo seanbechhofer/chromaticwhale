beab03bfcaca436d6374ad2416c700f8d2d77220
Sean Bechhofer <sean.bechhofer@manchester.ac.uk>
2016-11-11 16:04:45 +0000

    Minor updates

--- python/generate.py
+++ python/generate.py
@@ -13,6 +13,7 @@ import sparql
 parser = argparse.ArgumentParser(description='Generate Tracery Grammar.')
 parser.add_argument('-i', '--input', help='output file', default="grammar.json")
 parser.add_argument('-n', '--number', help='output file', type=int, default=4)
+parser.add_argument('--html', action="store_true")
 
 args = parser.parse_args()
 
@@ -21,6 +22,21 @@ with open(args.input) as f:
     
 grammar = tracery.Grammar(rules)
 grammar.add_modifiers(base_english)
+if args.html:
+    print "<html>"
+    print "<head><style>body {font-family: 'Open Sans';}</style></head>"
+    print "<body>"
+    
 for i in range(0,args.number):
+    if args.html:
+        print "<p>"
     print grammar.flatten("#origin#")
     print
+    if args.html:
+        print "</p>"
+    
+
+if args.html:
+    print "</body>"
+    print "</html>"
+    
